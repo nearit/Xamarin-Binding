7555fd7fecaaa5e4e73cfca581a2fd34186ca496
andrea.calegari <andrea@nearit.com>
2018-01-30 15:26:35 +0100

    android and ui bindings

---
+++ NearIT.Android/NearIT.Android/Additions/CouponReaction.cs
@@ -0,0 +1,14 @@
+﻿namespace IT.Near.Sdk.Reactions.Couponplugin
+{
+    public partial class CouponReaction 
+    {
+		protected override void NormalizeElement(global::Java.Lang.Object element)
+		{
+            NormalizeElement(element as Couponplugin.Model.Coupon);
+		}
+        protected override void InjectRecipeExtras(global::Java.Lang.Object element, IT.Near.Sdk.Trackings.TrackingInfo trackingInfo)
+		{
+            InjectRecipeExtras(element as IT.Near.Sdk.Reactions.Couponplugin.Model.Coupon, trackingInfo);
+		}
+    }
+}
---
+++ NearIT.Android/NearIT.Android/Additions/CustomJSONReaction.cs
@@ -0,0 +1,15 @@
+﻿
+namespace IT.Near.Sdk.Reactions.Customjsonplugin
+{
+    public partial class CustomJSONReaction 
+    {
+		protected override void NormalizeElement(global::Java.Lang.Object element)
+		{
+			NormalizeElement(element as IT.Near.Sdk.Reactions.Customjsonplugin.Model.CustomJSON);
+		}
+        protected override void InjectRecipeExtras(global::Java.Lang.Object element, IT.Near.Sdk.Trackings.TrackingInfo trackingInfo)
+		{
+            InjectRecipeExtras(element as IT.Near.Sdk.Reactions.Customjsonplugin.Model.CustomJSON, trackingInfo);
+		}
+    }
+}
