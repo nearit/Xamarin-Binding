52d087c0c2e1981cfbe28754ea37720caf358c27
Cattaneo Stefano <stefano@nearit.com>
2019-11-12 12:36:08 +0100

    fix(near): bump near version to latest

---
+++ NearIT.iOS/NearITSDK.framework/Headers/NITTrackingInfo.h
@@ -0,0 +1,26 @@
+//
+//  NITTrackingInfo.h
+//  NearITSDK
+//
+//  Created by francesco.leoni on 11/08/17.
+//  Copyright © 2017 NearIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#define KEY_DELIVERY_ID @"delivery_id"
+
+@interface NITTrackingInfo : NSObject<NSCoding>
+
+- (BOOL)addExtraWithObject:(id<NSCoding> _Nonnull)object key:(NSString* _Nonnull)key;
+- (NSString * _Nullable)recipeId;
+- (void)setRecipeId:(NSString * _Nonnull)recipeId;
+- (NSDictionary* _Nonnull)extrasDictionary;
+- (BOOL)existsExtraForKey:(NSString* _Nonnull)key;
+- (BOOL)hasDeliveryID;
+- (NSString* _Nullable)deliveryID;
+
++ (NITTrackingInfo* _Nonnull)trackingInfoFromRecipeId:(NSString* _Nonnull)recipeId;
++ (NITTrackingInfo* _Nonnull)trackingInfoFromRecipeId:(NSString* _Nonnull)recipeId deliveryId:(NSString* _Nullable)deliveryId;
++ (NITTrackingInfo* _Nonnull)trackingInfoFromRecipeId:(NSString* _Nonnull)recipeId extras:(NSDictionary<NSString*, id> *_Nonnull)extras;
+
+@end
